dd0e48e9b19ce573b7df1b674402804630d45ce9
troy <leeiam2.magsipoc@live.uwe.ac.uk>
2026-05-04 12:22:55 +0100

    AddingRT+SizeDIffs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private float spawnTime;
+    private float targetSize;
+    private bool wasHit = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
